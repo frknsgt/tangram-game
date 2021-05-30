d60da7f86c188e1303f5c5fff055bfb8102ca323
FurkanGULCAN <furkan7727@gmail.com>
2021-05-30 19:51:33 +0300

    feat: added about page

--- src/app/pages/client/about/about.component.ts
+++ src/app/pages/client/about/about.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { faGithub,faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
+
 })
 export class AboutComponent implements OnInit {
 
   constructor() { }
+  github=faGithub
+  instagram=faInstagram
 
   ngOnInit(): void {
   }
