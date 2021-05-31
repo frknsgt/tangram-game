65be67db43550f884c42902e6ac038072273c8e0
frknsgt <furkan.v.sk@gmail.com>
2021-05-31 13:25:12 +0300

    refactor: faulty page design revised for about page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -12,6 +12,34 @@ export class AboutComponent implements OnInit {
     private _activateRoute: ActivatedRoute
   ) {}
 
+  persons = [
+    {
+      Name: 'Furkan Söğüt',
+      Description: 'Full-Stack Developer',
+      ImagePath: '../../../../assets/img-about/sögüt.jpg',
+    },
+    {
+      Name: 'Ezgi Selin Kılıç',
+      Description: 'Frontend Developer',
+      ImagePath: '../../../../assets/img-about/ezgi.jpg',
+    },
+    {
+      Name: 'Ceyda Albayrak',
+      Description: 'Frontend Developer',
+      ImagePath: '../../../../assets/img-about/ceyda.jpg',
+    },
+    {
+      Name: 'Sefa Dedeoğlu',
+      Description: 'Full-Stack Developer',
+      ImagePath: '../../../../assets/img-about/sefa.jpg',
+    },
+    {
+      Name: 'Furkan Gülcan',
+      Description: 'Frontend Developer',
+      ImagePath: '../../../../assets/img-about/furkan2.jpg',
+    },
+  ];
+
   ngOnInit(): void {
     const id = this._activateRoute.snapshot.paramMap.get('id');
     if (id) this._router.navigate(['level', id]);
